b18f72cdc263a3f2d8e3ae2078436b00e63dca09
Hasmukh Rathod <guru__@hotmail.com>
2020-01-28 16:19:14 +0100

    games

---
+++ WebApplication1/Models/GuessingGame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class GuessingGame
+    {
+        public int EnterredNumber { get; set; }
+        public List<int> guesses = new List<int>();
+        public string message { get; set; }
+
+        public GuessingGame()
+        {
+            Random rand = new Random();
+            EnterredNumber = rand.Next(1, 100);
+
+        }
+        public void AddGuessNumber(int num)
+        {
+            if (num > EnterredNumber)
+            {
+                message = "you´r number is to high ";
+
+            }
+            else if (num < EnterredNumber)
+            {
+                message = "you´r number is low";
+            }
+
+            else if (num == EnterredNumber)
+            {
+                message = "you´ number is correct );";
+            }
+
+            guesses.Add(num);
+
+        }
+       
+    }
+}
